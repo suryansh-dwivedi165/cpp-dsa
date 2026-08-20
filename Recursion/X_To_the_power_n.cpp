92fd3d9852089eb34f9520e0e324f0be6f6d5dcb
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-08-20 19:58:21 +0530

    .\X_To_the_power_n.cpp

---
+++ Recursion/X_To_the_power_n.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int base = 3;
+    int power = 3;
+    int base2 = 1;
+
+    for(int i = 0;i < power;i++) {
+        base2 *= base;
+    }
+    cout << base << " to the Power of " << power << " is equal to: "<< base2 << " ";
+    return 0;
+} 
