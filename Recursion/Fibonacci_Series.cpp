d1a8acdca2e0be2c3d91fefe0f535695e977b27e
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-08-25 22:05:37 +0530

    .\Fibonacci_Series.cpp

---
+++ Recursion/Fibonacci_Series.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fib(int n) {
+    if(n == 0 || n == 1) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+int main() {
+    int n = 6;
+    int ans = fib(n);
+
+    cout << "Fibnocci number is: " << ans;
+    return 0;
+} 
