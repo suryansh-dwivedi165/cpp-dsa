8940fe1b5fb06ae52e507fbbb5490a0c977a8273
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-08-26 23:07:46 +0530

    .\last_occurence.cpp

--- Recursion/last_occurence.cpp
+++ Recursion/last_occurence.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int last_occurence(int arr[], int n, int target) {
-    if(arr[n - 1] == target) 
-        return n;
-    last_occurence(arr, n - 1, target);
-    if(n == 0) 
+int last_occurence(vector<int>arr, int target) {
+    if(arr[arr.size() - 1] == target) 
+        return arr.size();
+    last_occurence(arr, target);
+    if(arr.size() == 0) 
         return -1;
 }
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    vector<int>arr = {1, 2, 3, 4, 5};
 
-    int ans = last_occurence(arr, 5, 3);
+    int ans = last_occurence(arr, 3);
     cout << "Last occurence idx: " << ans;
     return 0;
-} 
+}  
