bea7f6c929fca5e4b71b845cdf286c33163f6444
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-09-01 22:41:23 +0530

    .\Merge_sort.cpp

---
+++ Sorting/Merge_sort.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int merge_sort(int arr[], int n) {
+    return n;
+}
+int main() {
+    int arr[] = {6, 3, 7, 5, 2, 4};
+
+    int ans = merge_sort(arr, 6);
+    cout << "Ans is: " << ans;
+    return 0;
+} 
