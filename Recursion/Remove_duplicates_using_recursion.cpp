cdae3de4101ddeac921ec8b3d32014fb8e53e1a1
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-09-04 10:33:34 +0530

    .\Remove_duplicates .exe

--- Recursion/Remove_duplicates_using_recursion.cpp
+++ Recursion/Remove_duplicates_using_recursion.cpp
@@ -1,15 +1,33 @@
 #include<iostream>
 #include<string>
-using namespace std;
-void remove_duplicates(string str) {
-    
-}
+using namespace std; 
+void remove_duplicates(string str, string ans, int i, bool map[]) {
+    if(i == str.length()) {
+        cout << ans;
+        return;
+    }
+
+    char ch = str[i];
+    int mapidx = int(ch - 'a');
+
+    if(map[mapidx] == true) {
+        remove_duplicates(str, ans, i + 1, map);
+    }
+    else {
+        map[mapidx] = true;
+        remove_duplicates(str, ans + str[i], i + 1, map);
+    }
+} 
 int main() {
     string str = "appnnacollege";
+    string ans = "";
+    bool map[26] = {false}; 
+    int i = 0;
 
     for(int i = 0;i < str.length();i++) {
         str[i] = char(tolower(str[i]));
     }
-    remove_duplicates(str);
+    remove_duplicates(str, ans, i, map);
+    
     return 0; 
-} 
+}    
