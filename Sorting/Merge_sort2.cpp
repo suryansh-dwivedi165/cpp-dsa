d2082a8d469ba81b5fb754a5568f9a24024f9fdf
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-09-12 04:02:11 +0530

    .\Merge_sort.cpp

--- Sorting/Merge_sort2.cpp
+++ Sorting/Merge_sort2.cpp
@@ -1,12 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int arr[] = {6, 3, 7, 5, 2, 4};
+void merge(int arr[], int si, int mid, int ei) {
+    vector<int>temp;    
+    int i = si;
+    int j = ei;
 
-    sort(arr, arr + 6);
-    
-    for(int i = 0;i < 6;i++) {
+    while(i <= mid && j <= ei) {
+        if(arr[i] <= arr[j]) {
+            temp.push_back(arr[i++]);
+        }else {
+            temp.push_back(arr[j++]);
+        }
+    }
+
+    while(i <= mid) {
+        temp.push_back(arr[i++]);
+    }
+
+    while(j <= ei) {
+        temp.push_back(arr[j++]);
+    }
+}
+
+void mergeSort(int arr[], int si, int ei) {
+    if(si <= ei) {
+        return;
+    }
+
+    int mid = si + (si - ei) / 2;
+
+    mergeSort(arr, si, mid);
+    mergeSort(arr, mid + 1, ei);
+}
+
+void printArr(int arr[], int n) {
+    for(int i = 0;i < n;i++) {
         cout << arr[i] << " ";
     }
+}
+int main() { 
+    int arr[] = {6, 3, 7, 5, 2, 4};
+    int n = 6;
+
+    mergeSort(arr, 0, n - 1);
+    printArr(arr, n);
     return 0;
-} 
+}   
