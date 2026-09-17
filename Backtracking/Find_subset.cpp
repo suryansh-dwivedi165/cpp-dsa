f6a1300a619422de4ebe5e56f9d8f9765e4afaa8
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-09-17 20:52:48 +0530

    .\Find_subset.cpp

--- Backtracking/Find_subset.cpp
+++ Backtracking/Find_subset.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
+void printsubsets(string str, string ans) {
+    if(str.size() == 0) {
+        cout << ans << "\n";
+        return;
+    }
+    
+    char ch = str[0];
+    printsubsets(str.substr(1, str.length() - 1), ans + ch);
+
+    printsubsets(str.substr(1, str.length() - 1), ans); 
+}
 int main() {
     string str = "abc";
-    int len = str.length();
+    string ans = "";
 
-    cout << len << " ";
+    printsubsets(str , ans);
     return 0;
 } 
