16afa9d763922f8e22b734551ee48c536ea8b0c6
suryansh-dwivedi165 <suryanshdwivedi39@gmail.com>
2026-09-22 23:18:36 +0530

    .\Array_traversal.cpp

--- Backtracking/Array_traversal.cpp
+++ Backtracking/Array_traversal.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int *changeArr(int arr[], int n, int i) {
-    if(i == n) {
-        return arr;
-    }
-    arr[i] = i + 1;
-    changeArr(arr, n, i + 1);
-    arr[i] = arr[i] - 2;
-} 
+// int *changeArr(int arr[], int n, int i) {
+//     if(i == n) {
+//         return arr;
+//     }
+//     arr[i] = i + 1;
+//     changeArr(arr, n, i + 1);
+//     arr[i] = arr[i] - 2;
+// } 
 
-int main() {
-    int arr[5] = {0};
-    int *ptr = changeArr(arr, 5, 0);
+// int main() {
+//     int arr[5] = {0};
+//     int *ptr = changeArr(arr, 5, 0);
 
-    for(int i = 0;i <= sizeof(ptr);i++) {
-        cout << ptr[i] << " ";
-    }
-} 
+//     for(int i = 0;i <= sizeof(ptr);i++) {
+//         cout << ptr[i] << " ";
+//     }
+// } 
